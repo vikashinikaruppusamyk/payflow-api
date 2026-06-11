f80ae6516c18004f7f652099ea61b88b77bc1f14
Vikashini <vikashinikaruppusamyk@gmail.com>
2026-06-11 17:33:17 +0530

    Update User entity to map to app_user table
    - Add @Table annotation to map User entity to app_user table
    - Resolve reserved keyword issue with table name

--- src/main/java/com/example/payflow/entity/User.java
+++ src/main/java/com/example/payflow/entity/User.java
@@ -3,6 +3,7 @@ package com.example.payflow.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
