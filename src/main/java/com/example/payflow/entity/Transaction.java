8ebd3a8c924a24e118f3e556ea0f32b2bfa06c60
Vikashini <vikashinikaruppusamyk@gmail.com>
2026-06-11 17:47:17 +0530

    Update Transaction entity to map to transaction table

--- src/main/java/com/example/payflow/entity/Transaction.java
+++ src/main/java/com/example/payflow/entity/Transaction.java
@@ -1,11 +1,9 @@
 package com.example.payflow.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "transaction")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
